abd8ce027b3b2cf6066cf87a0e5831fba8128136
huuudrich <108597096+huuudrich@users.noreply.github.com>
2023-04-19 19:37:38 +1100

    feat: create database tables

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name  VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS requests
+(
+    id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    description  VARCHAR(255) NOT NULL,
+    requester_id BIGINT,
+    FOREIGN KEY (requester_id) REFERENCES users (id),
+    UNIQUE (id)
+);
+
+CREATE TABLE IF NOT EXISTS items
+(
+    id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name         VARCHAR(255) NOT NULL,
+    description  VARCHAR(255) NOT NULL,
+    is_available BOOLEAN,
+    owner_id     BIGINT,
+    request_id   BIGINT,
+    FOREIGN KEY (owner_id) REFERENCES users (id),
+    FOREIGN KEY (request_id) REFERENCES requests (id)
+);
+
+CREATE TABLE IF NOT EXISTS bookings
+(
+    id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    start_date TIMESTAMP WITHOUT TIME ZONE,
+    end_date   TIMESTAMP WITHOUT TIME ZONE,
+    item_id    BOOLEAN,
+    booker_id  BIGINT,
+    status     BOOLEAN,
+    FOREIGN KEY (booker_id) REFERENCES users (id),
+    FOREIGN KEY (item_id) REFERENCES items (id)
+);
+
+CREATE TABLE IF NOT EXISTS comments
+(
+    id        BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    text      VARCHAR(255) NOT NULL,
+    item_id   BIGINT,
+    author_id BIGINT,
+    FOREIGN KEY (author_id) REFERENCES users (id),
+    FOREIGN KEY (item_id) REFERENCES requests (id)
+);
